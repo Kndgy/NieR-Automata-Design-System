d587df87a0bf0d7d1949d6b053517cc49b3345de
Rafli Ghani <ghanirafli8@gmail.com>
2022-06-29 00:23:43 +0700

    navbar without routing yet

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,46 @@
 import React from 'react';
 import Map from './pages/map';
 import styles from './App.module.scss';
+import { Bar, Button } from '@kaineee/nier-automata-ui-library';
+
+const test = {
+  testjson:[
+    {
+      Text:"MAP",
+      
+    },
+    {
+      Text:"QUESTS"
+    },
+    {
+      Text:"ITEMS"
+    },
+    {
+      Text:"WEAPONS"
+    },
+    {
+      Text:"SKILLS"
+    },
+    {
+      Text:"INTEL"
+    },
+    {
+      Text:"SYSTEM"
+    }
+  ]
+}
 
 function App() {
   return (
     <div className={styles.App}>
+      <div className={styles.NavBar}>
+        <Bar/>
+        {test.testjson.map((item,key)=>{
+          return(
+            <Button key={key} text={item.Text} />
+          )
+        })}
+      </div>
       <Map />
     </div>
   );
