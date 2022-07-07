e03b6fac6b0c6efa1ec7eb5bb0920269c21a3a29
Rafli Ghani <ghanirafli8@gmail.com>
2022-07-07 22:37:04 +0700

    add rollup

---
+++ rollup.config.js
@@ -0,0 +1,49 @@
+import json from '@rollup/plugin-json'
+import esbuild from 'rollup-plugin-esbuild'
+import dts from 'rollup-plugin-dts'
+
+export default [
+  {
+    input: "./src/components/index.ts",
+    output: [
+      {
+        file: "dist/index.js",
+        format: "cjs",
+      },
+      {
+        file: "dist/index.es.js",
+        format: "es",
+        exports: "named",
+      },
+    ],
+    plugins: [
+      json(),
+      esbuild({
+        include: /\.[jt]sx?$/,
+        exclude: /node_modules/,
+        sourceMap: false,
+        minify: process.env.NODE_ENV === 'production',
+        target: 'esnext',
+        jsx: 'transform',
+        jsxFactory: 'React.createElement',
+        jsxFragment: 'React.Fragment',
+        define: {
+          __VERSION__: '"0.1.0"',
+        },
+        tsconfig: 'tsconfig.json',
+        loaders: {
+          '.json': 'json',
+          '.js': 'jsx',
+        },
+      }),
+    ],
+  },
+  {
+    input :"./src/components/index.ts",
+    output:{
+      file: "dist/index.d.ts",
+      format:'es',
+    },
+    plugins: [dts()]
+  },
+];
