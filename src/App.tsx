11f7653104a3083fda65daeb08246e492a1b5e1f
Rafli Ghani <ghanirafli8@gmail.com>
2022-07-09 03:57:51 +0700

    import name

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './App.module.scss';
 import { RoutesPages } from './routes/Routes';
-import NavbarModule from './modules/navBarModule';
+import NavbarModule from './modules/NavBarModule';
 
 function App() {
   return (
