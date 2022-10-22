00b982a88f7241b01b45e34083b9bde53b09b004
Rafli Ghani <ghanirafli8@gmail.com>
2022-10-22 22:06:44 +0700

    line elements

--- src/components/DropDown.tsx
+++ src/components/DropDown.tsx
@@ -39,8 +39,43 @@ font-size: 18px;
   background-position: -100%;
   color: #57544a;
 }
+/* &::before{
+  height: 0px;
+  width: 100%;
+  background-color: #57544a;
+  content: "";
+  transform: translate(0px, 0px);
+  transition: 0.2s;
+  pointer-events: none;
+}
+&::after{
+  height: 0px;
+  width: 100%;
+  background-color: #57544a;
+  content: "";
+  transform: translate(0px, 0px);
+  transition: 0.2s;
+  z-index: -1;
+  pointer-events: none;
+}
+&:hover{
+  &::before{
+    height: 2px;
+    width: 100%;
+    content: "";
+    transform: translate(0px, -8px);
+    pointer-events: none;
+}
+  &::after{
+    height: 2px;
+    z-index: -1;
+    width: 100%;
+    content: "";
+    transform: translate(0px, 8px);
+    pointer-events: none;
+  }
+} */
 `
-
 
 export const DropDown = ({Title, Content}: DropDownTypes) => {
 
@@ -54,7 +89,8 @@ export const DropDown = ({Title, Content}: DropDownTypes) => {
     <details>
       <StyledSummary 
         onClick={handleClick} 
-        style={{backgroundPosition: isActive ? '-100%' : '', color: isActive ? '#b4af9a' :''}}
+        style={{backgroundPosition: isActive ? '-100%' : '', 
+        color: isActive ? '#b4af9a' :''}}
       >
         <Symbol 
           style={{display:"flex", justifyContent:"center", 
