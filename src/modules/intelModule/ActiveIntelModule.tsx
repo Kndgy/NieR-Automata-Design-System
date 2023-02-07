a21b53b55d0ed094727215a5a5edaa07a6756677
kaine <ghanirafli8@gmail.com>
2023-02-07 19:54:00 +0700

    archives type checker

--- src/modules/intelModule/ActiveIntelModule.tsx
+++ src/modules/intelModule/ActiveIntelModule.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { Tab, Widget } from "../../components";
-import {getArchivesMockID, getNestedArchivesMockID } from "../../utils/mockData/archivesMockData";
+import {getArchivesMockID, getNestedArchivesMockID, getArchivesMockData } from "../../utils/mockData/archivesMockData";
 
 
 export const ActiveIntelModule = () => {
   const params = useParams();
   
-  let intellistId = getArchivesMockID(parseInt(params.intelid,10));
-  let secondId = getNestedArchivesMockID(parseInt(params.intelid, 10))
+  let intellistId = getArchivesMockID(parseInt(params.intelid));
+  let secondId = getNestedArchivesMockID(parseInt(params.intelid))
+  let intellist = getArchivesMockData();
   // console.log(params);
   
   const yeah = intellistId[0].filter(e => e!==undefined)
@@ -17,6 +18,12 @@ export const ActiveIntelModule = () => {
   const x = yeah.concat(yeahaa)
   const data = x[0]
   console.log(data.content)
+  const ArchivesTypeCheck = () => {
+    if(intellist.map((type)=>type.IntelType == "Archives")){
+      return "yeah it is archives"
+    }
+  }
+  console.log(ArchivesTypeCheck())
 
   //might want to write better function to check content type
   const imageCheck = () => {
