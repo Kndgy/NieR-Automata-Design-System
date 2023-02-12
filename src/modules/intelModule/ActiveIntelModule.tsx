ee7a9a5dc88a866a3ba2006108db829afb0ca84c
kaine <ghanirafli8@gmail.com>
2023-02-12 19:53:46 +0700

    streamline active intel module info

--- src/modules/intelModule/ActiveIntelModule.tsx
+++ src/modules/intelModule/ActiveIntelModule.tsx
@@ -2,28 +2,23 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { Tab, Widget } from "../../components";
 import {getArchivesMockID, getNestedArchivesMockID} from "../../utils/mockData/archivesMockData";
-import { eliminateUndefined } from "../../utils/eliminateUndefinedJSON";
 import { Archives, Fishing, Novel, PictureBooks, Tutorials, Unit, WeaponStories } from "./shards";
 
 export const ActiveIntelModule = () => {
-  const params = useParams();
-  let intellistId = getArchivesMockID(parseInt(params.intelid));
-  let secondId = getNestedArchivesMockID(parseInt(params.intelid))
-  const filteredIntelList = intellistId[0].filter(e => e!==undefined)
-  let result = eliminateUndefined(secondId)
-  if(result === null){
-    result = ""
-  }else{
-    result = result[0][0][0]
-  }
-  const data = filteredIntelList.concat(result)[0]
 
+  const params = useParams();
+  const paramType = params.type
+
+  let intellistId = getArchivesMockID(paramType, parseInt(params.intelid));
+  let secondId = getNestedArchivesMockID( paramType, parseInt(params.intelid))
+
+  const data = intellistId || secondId
   console.log(data)
 
   const ArchivesTypeCheck = () => {
     const typeMap = {
-        archives: data ? (
-            <Archives content={data.content}/>
+        archives: 1 ? (
+            <Archives content={"test"}/>
             ) : "weird it seems the data is empty",
         unit: <Unit/>,
         tutorials: <Tutorials/>,
