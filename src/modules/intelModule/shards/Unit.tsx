2ae1da38c972ae354e215ceba0abc5e7783b7fa2
kaine <ghanirafli8@gmail.com>
2023-02-14 21:17:44 +0700

    unit data placeholder

--- src/modules/intelModule/shards/Unit.tsx
+++ src/modules/intelModule/shards/Unit.tsx
@@ -1,8 +1,31 @@
+import { useParams } from "react-router-dom";
+import { getArchivesMockID, getNestedArchivesMockID } from "../../../utils/mockData/archivesMockData";
 
 export const Unit = () => {
- return (
-  <>
-   Unit
-  </>
- )
+    const params = useParams();
+    const paramType = params.type
+  
+    let intellistId = getArchivesMockID(paramType, parseInt(params.intelid));
+    let secondId = getNestedArchivesMockID( paramType, parseInt(params.intelid))
+
+    const data = intellistId || secondId
+
+    // console.log(data.content)
+    
+    const mapCheck = () => {
+        if (!data.hasOwnProperty("content")) {
+            return <>weird, it seems the unit data is empty</>;
+        }
+    
+        switch (true) {
+            case data.hasOwnProperty("content"):
+                return <> {data.content} </>
+            default:
+                return <>weird, it seems the unit data is empty</>;
+        }
+    }
+
+    return (
+        <> {mapCheck()} </>
+    )
 }
